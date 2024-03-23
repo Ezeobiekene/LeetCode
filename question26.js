9df1cf0f5705e6e03bc8a3dbdc3c5316dec435bf
Ezeobiekene <ezeobiekene7@gmail.com>
2024-03-23 13:22:00 -0400

    solved question 26

---
+++ question26.js
@@ -0,0 +1,11 @@
+var removeDuplicates = function(nums) {
+    let index = 1
+    for(let i = 1; i < nums.length; i++){
+        if(nums[i] !== nums[i-1]){
+            nums[index] = nums[i]
+            index++
+        }
+    }
+
+    return index
+};
