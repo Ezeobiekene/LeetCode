f956d79dedf277ccabdf5e9401c64ccd9814b3ad
Ezeobiekene <ezeobiekene7@gmail.com>
2024-03-23 13:36:41 -0400

    solved question 26 and added comments

--- question26.js
+++ question26.js
@@ -1,8 +1,38 @@
+
+//This is better for the language JavaScript
 var removeDuplicates = function(nums) {
-    let index = 1
+
+//looping through the array 
     for(let i = 1; i < nums.length; i++){
+        //since the array is sorted then if any number is the same as the number before it then it is a duplicate
+        if(nums[i] === nums[i-1]){
+            //remove the duplicate
+            nums.splice(i, 1)
+
+            //decreament to reset the index counter because we removed an element
+            i--
+        }
+    }
+
+    return nums.length
+};
+
+
+
+//This is more accurate to the question
+var removeDuplicates = function(nums) {
+    //initialize the index variable
+    let index = 1
+
+    //loop through the array
+    for(let i = 1; i < nums.length; i++){
+
+        //if the current element is not the same as the previous element then it is not a duplicate
         if(nums[i] !== nums[i-1]){
+
+            //set the current element to be in the index position
             nums[index] = nums[i]
+            //increament the index for each unique element
             index++
         }
     }
